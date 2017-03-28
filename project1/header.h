92ea6e901f1b7198aa7f5cff3b92eed9ce2d1933
ckavadias <ckavadias@50708fc9-c562-4be5-be18-e52b07e82b13>
2017-03-28 10:16:44 +0000

    project1 v4 architecture facelift

--- project1/header.h
+++ project1/header.h
@@ -15,50 +15,53 @@ and system variables during simulation*/
 
 /*Struct Definitions*/
 
+typedef struct process process_t;
+
 //queue definition for queue admin
 typedef struct {
-	void* start;
-	void* end;
+	process_t* start;
+	process_t* end;
 	int num_inqueue;
 }queue_t;
 
 //Struct definitions for system variables
-typedef struct memhole memhole_t;
 
-struct memhole{
+typedef struct{
 	int address;
 	int size;
-	memhole_t* next;
-	memhole_t* previous;
-};
+}memhole_t;
 
 typedef struct {
-	queue_t* memholes;
 	int numprocesses;
 	int memusage;
 	int quantum;
 	int max_memory;
+	int num_holes;
+	memhole_t* memholes;
 	void (*add_list)(queue_t*, memhole_t*);
 }system_t;
 
 //process structs
-typedef struct process process_t;
-
 struct process{
-	int next_arrival;
+	int arrival;
 	int id;
 	int memory_needed;
 	int burst_left;
-	int loaded;
+	int address;
+	process_t* next_mem;
 	process_t* next;
-	process_t* prev;
+	process_t* next_disk;
+	process_t* next_arrival;
 };
 
 /*Function prototypes*/
 void debug(int number, char* message);
 void debug_process(process_t* process);
-void process_queue(queue_t* queue, FILE* openfile);
+void arrival_queue(queue_t* queue, FILE* openfile);
 void initialise_system(system_t* system, queue_t* queue,int argc,char* argv[]);
 void worst_fit(queue_t* queue, memhole_t* memhole);
 void first_fit(queue_t* queue, memhole_t* memhole);
 void best_fit(queue_t* queue, memhole_t* memhole);
+int null_check(void* pointer, int terminate);
+void initialise_memholes(system_t* system);
+void remove_mem(queue_t* queue, process_t* process);

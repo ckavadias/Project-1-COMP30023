27c65ec83ad72dc8f032596624a98a5212d91f76
ckavadias <ckavadias@50708fc9-c562-4be5-be18-e52b07e82b13>
2017-03-31 06:04:39 +0000

    project1 v5 all functions written

--- project1/header.h
+++ project1/header.h
@@ -37,8 +37,9 @@ typedef struct {
 	int quantum;
 	int max_memory;
 	int num_holes;
-	memhole_t* memholes;
-	void (*add_list)(queue_t*, memhole_t*);
+	int clock;
+	memhole_t** memholes;
+	int (*sort_list)(void*, void*);
 }system_t;
 
 //process structs
@@ -59,9 +60,28 @@ void debug(int number, char* message);
 void debug_process(process_t* process);
 void arrival_queue(queue_t* queue, FILE* openfile);
 void initialise_system(system_t* system, queue_t* queue,int argc,char* argv[]);
-void worst_fit(queue_t* queue, memhole_t* memhole);
-void first_fit(queue_t* queue, memhole_t* memhole);
-void best_fit(queue_t* queue, memhole_t* memhole);
+int worst_fit(void* parent, void* child);
+int first_fit(void* parent, void* child);
+int best_fit(void* parent, void* child);
 int null_check(void* pointer, int terminate);
 void initialise_memholes(system_t* system);
-void remove_mem(queue_t* queue, process_t* process);
+process_t* remove_mem(queue_t* queue, process_t* process);
+process_t* load(queue_t* disk, system_t* system, queue_t* memory, queue_t* rr);
+void add_rr(queue_t* queue, process_t* process);
+void find_hole(system_t* system, process_t* process, queue_t* memory,
+										   queue_t* disk, queue_t* round_robin);
+void swap(void** memholes, int first, int second);
+void sift(void** memholes, int start, int end, 
+							int (*sort_list)(void*, void*));
+void heapify(int size, void** memholes,
+								   int (*sort_list)(void*, void*));
+void heapsort_t(void** memholes, int size, 
+								int (*sort_list)(void*, void*));
+void add_hole(int address, int size, system_t* system);
+void delete_hole(memhole_t** memholes, int index, int size);
+void check_contiguity(system_t* system);
+void add_disk(process_t* process, queue_t* disk);
+int memusage(queue_t* memory, system_t* system);
+int p_sort(void* process1, void* process2);
+process_t** p_array(process_t* process, process_t** array, int index, int* max);
+void remove_rr(queue_t* queue, process_t* process);

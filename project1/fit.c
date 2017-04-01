032df92b2043204239b3dd0596c327e22dc366ca
ckavadias <ckavadias@50708fc9-c562-4be5-be18-e52b07e82b13>
2017-04-01 09:59:33 +0000

    project1 v6 debugged, passed test cases

--- project1/fit.c
+++ project1/fit.c
@@ -45,15 +45,16 @@ int worst_fit(void* parent, void* child){
 //and https://tinyurl.com/keq7e4q
 void heapsort_t(void** memholes, int size, 
 								int (*sort_list)(void*, void*)){
-	
+	debug(0, "__heapsort start");
 	//heapify the array
 	heapify(size, memholes, sort_list);
 	
 	while(size > 0){
-		swap(memholes, size, 0);
+		swap(memholes, size - 1, 0);
 		size--;
-		sift(memholes, 0, size, sort_list);
+		sift(memholes, 0, size -1, sort_list);
 	}
+	debug(0, "__heapsort end");
 }
 
 //restore heap property to an array
@@ -61,18 +62,20 @@ void heapify(int size, void** memholes,
 								   int (*sort_list)(void*, void*)){
 	int start = (size-1)/2;
 	
+	debug(0, "__heapify start");
 	while(start >= 0){
 		sift(memholes, start, size -1, sort_list);
 		start--;
 	}
-	
+	debug(0, "__heapify end");
 }
 
 //sifting function to assist with heapifying
 void sift(void** memholes, int start, int end, 
 							int (*sort_list)(void*, void*)){
 	int child, root;
-
+	
+	debug(0, "		__sift start");
 	while(2*start + 1 <= end){
 		child = 2*start + 1;
 		root = start;
@@ -84,12 +87,14 @@ void sift(void** memholes, int start, int end,
 			root = child + 1;
 		}
 		if(root == start){
+			debug(0, "		__sift end at start");
 			return;
 		}
 		
 		swap(memholes, start, root);
 		start = root;
 	}
+	debug(0, "		__sift end");
 	
 }
 
@@ -97,17 +102,18 @@ void sift(void** memholes, int start, int end,
 void swap(void** memholes, int first, int second){
 	memhole_t* temporary;
 	
+	debug(0, "		__swap start");
 	temporary = memholes[first];
 	memholes[first] = memholes[second];
 	memholes[second] = temporary;
-	
+	debug(0, "		__swap end");
 	return;
 }
 
 //sort processes based on priority
 int p_sort(void* process1, void* process2){
 	
-	if(((process_t*)process1)->id > ((process_t*)process2)->id){
+	if(((process_t*)process1)->id < ((process_t*)process2)->id){
 		return 1;
 	}
 	
--- project1/sys_admin.c
+++ project1/sys_admin.c
@@ -16,8 +16,18 @@ extern char* optarg;
 //debugging integers
 void debug(int number, char* message){
 	if(DEBUG){
-		fprintf(stderr, "%s %d\n", message, number);
+		dopen("debug.txt", "a");
+		fprintf(debug_f, "%s %d\n", message, number);
+		dclose(debug_f);
 	}
+}
+
+//open and close debugging record file
+void dopen(char* name, char* mode){
+	debug_f = fopen(name, mode);
+}
+void dclose(FILE* file){
+	fclose(file);
 }
 
 //print out contents of process for debugging
@@ -30,7 +40,7 @@ void debug_process(process_t* process){
 	}
 }
 
-//function to create queue of process
+//function to create queue of processes
 void process_queue(queue_t* queue, FILE* openfile){
 	process_t* current;
 	
@@ -46,12 +56,12 @@ void process_queue(queue_t* queue, FILE* openfile){
 		&(current->id), &(current->memory_needed), &(current->burst_left));
 		
 		current->address = -1;
-		current->next = (process_t*)malloc(sizeof(process_t));
-		null_check(current->next, 1);
+		current->next_arrival = (process_t*)malloc(sizeof(process_t));
+		null_check(current->next_arrival, 1);
 		queue->num_inqueue++;
 		
 		queue->end = current;
-		current = current->next;
+		current = current->next_arrival;
 		debug(0, "still looping");
 	}
 	
@@ -87,13 +97,16 @@ void initialise_system(system_t* system, queue_t* queue,int argc,char* argv[]){
 					break;
 					
 				case 'a':
-					if(strcmp(optarg, "best")){
+					if(strcmp(optarg, "best") == 0){
+							debug(0, "best_fit assigned");
 							system->sort_list = (*best_fit);
 					}
-					else if(strcmp(optarg, "first")){
+					else if(strcmp(optarg, "first") == 0){
+							debug(0, "first_fit assigned");
 							system->sort_list = (*first_fit);
 					}
-					else if(strcmp(optarg, "worst")){
+					else if(strcmp(optarg, "worst") == 0){
+							debug(0, "best_fit assigned");
 							system->sort_list = (*worst_fit);
 					}
 					else{
@@ -120,13 +133,16 @@ void initialise_system(system_t* system, queue_t* queue,int argc,char* argv[]){
 	fclose(openfile);
 	
 	//create memholes list, maximum number of holes is size of memory
+	debug(0, "start initialising memholes array");
   system->memholes = (memhole_t**)malloc(sizeof(memhole_t*)*system->max_memory);
 	null_check(system->memholes, 1);
 	system->num_holes = 1;
 	system->clock = 0;
+	(system->memholes)[0] = (memhole_t*)malloc(sizeof(memhole_t));
+	null_check((system->memholes)[0], 1);
 	(system->memholes[0])->address = system->max_memory;
 	(system->memholes[0])->size = system->max_memory;
-	
+	debug(0, "__initialise_system finished");
 	
 }
 
@@ -147,6 +163,7 @@ int null_check(void* pointer, int terminate){
 process_t* remove_mem(queue_t* queue, process_t* process){
 	process_t* current, *previous;
 	
+	debug(0, "__remove_mem start");
 	current = queue->start;
 	previous = NULL;
 	
@@ -156,17 +173,23 @@ process_t* remove_mem(queue_t* queue, process_t* process){
 			
 			//check if start of queue
 			if(null_check(previous, 0)){
+				debug(0, "		inside queue");
 				previous->next_mem = current->next_mem;
 			}
 			else{
+				debug(0, "		start of queue");
+				debug(null_check(current, 0), "		void status of current");
 				queue->start = current->next_mem;
+				debug(0, "		current removed");
 				
 			}
 			if (!null_check(current->next_mem, 0)){
 				queue->end = previous;
-				previous->next = NULL;
+				
+				if(null_check(previous, 0)){
+						previous->next = NULL;
+				}
 			}
-			current->address = -1;
 			current->next_mem = NULL;
 			break;
 		}
@@ -175,6 +198,7 @@ process_t* remove_mem(queue_t* queue, process_t* process){
 		current = current->next_mem;
 	}
 	queue->num_inqueue--;
+	debug(0, "__remove_mem end");
 	return process;
 }
 
@@ -182,15 +206,17 @@ process_t* remove_mem(queue_t* queue, process_t* process){
 process_t* load(queue_t* disk, system_t* system, queue_t* memory, queue_t* rr){
 	process_t* process;
 	
+	debug(0, "__load start");
 	process = disk->start;
 	
 	if(null_check(process, 0)){
+		debug(0, "		process not NULL");
 		
 		//find hole
 		find_hole(system, process, memory, disk, rr);
 		
 		//add process to memory
-		if(memory->start == NULL && memory->end == NULL){
+		if(!memory->num_inqueue){
 			memory->start = process;
 			memory->end = process;
 			process->next_mem = NULL;
@@ -208,20 +234,30 @@ process_t* load(queue_t* disk, system_t* system, queue_t* memory, queue_t* rr){
 			memory->end = process;
 			process->next_mem = NULL;
 		}
-		disk->start = disk->start->next_disk;
-		process->next_disk = NULL;
+		
+		if(disk->start == disk->end){
+			disk->start = NULL;
+			disk->end = NULL;
+		}
+		else{
+			disk->start = disk->start->next_disk;
+			process->next_disk = NULL;
+		}
+		disk->num_inqueue--;
 		memory->num_inqueue++;
 		
-	printf("time %d, %d loaded, numprocesses=%d, numholes=%d, memusage=%d%\n",
+	printf("time %d, %d loaded, numprocesses=%d, numholes=%d, memusage=%d%%\n",
 		system->clock, process->id, memory->num_inqueue, system->num_holes,
 		memusage(memory, system));
 	}
-	
+	debug(0, "__load end");
 	return process;
 }
 
 //add process loaded from disk to round robin queue
 void add_rr(queue_t* queue, process_t* process){
+	
+	debug(0, "__add_rr start");
 	//process is void
 	if(!null_check(process, 0)){
 		return;
@@ -229,8 +265,10 @@ void add_rr(queue_t* queue, process_t* process){
 	
 	//process not void
 	else{
+		debug(0, "process not void");
 		//queue is empty
 		if(queue->start == NULL && queue->end == NULL){
+			debug(0, "		empty rr queue");
 			queue->start = process;
 			queue->end = process;
 			process->next = NULL;
@@ -244,6 +282,7 @@ void add_rr(queue_t* queue, process_t* process){
 		
 		//queue is not empty
 		else{
+			debug(0, "		rr queue not empty");
 			queue->end->next = process;
 			queue->end = process;
 			process->next = NULL;
@@ -251,27 +290,35 @@ void add_rr(queue_t* queue, process_t* process){
 		
 		queue->num_inqueue++;
 	}
+	debug(0, "__add_rr finish");
 }
 //find hole to place process in memory or create if necesary
 void find_hole(system_t* system, process_t* process, queue_t* memory,
 										   queue_t* disk, queue_t* round_robin){
 	process_t* removed_p = NULL, **removed;
-	int found = 0, i, address, size, num_disk = 0, priority = 0, max = 10;
+	int found = 0, i, address, size, num_disk = 0, max = 10;
 	
-	
+	debug(0, "__find_hole start");
 	removed = (process_t**)malloc(sizeof(process_t*)*max);
 	null_check(removed, 1);
+	
 	
 	while(!found){
 		
 		for(i = 0; i < system->num_holes; i++){
+			debug(system->num_holes, "find_hole inner for loop active");
 			if(process->memory_needed <= (system->memholes[i])->size){
+				debug(0,"		hole found");
+				process->address = (system->memholes[i])->address;
+				
+				debug(process->address, "		process address: ");
 				
 				if(process->memory_needed < (system->memholes[i])->size){
-					
+					debug(0, "		hole more than needed");
 					size = (system->memholes[i])->size - process->memory_needed;
-					address = (system->memholes[i])->address + 
+					address = (system->memholes[i])->address - 
 														process->memory_needed;
+														
 					delete_hole(system->memholes, i, system->num_holes);
 					system->num_holes--;
 					add_hole(address, size, system);
@@ -284,6 +331,7 @@ void find_hole(system_t* system, process_t* process, queue_t* memory,
 				}
 				
 				found = 1;
+				break;
 			}
 		}
 		
@@ -292,9 +340,11 @@ void find_hole(system_t* system, process_t* process, queue_t* memory,
 		}
 		
 		else{
-			
+			debug(0, "no hole found");
 			removed_p = remove_mem(memory, memory->start);
-			
+			debug(num_disk,"removed_p assigned");
+			//debug(removed_p->address, "remove_p address: ");
+			null_check(removed_p, 1);
 			add_hole(removed_p->address, removed_p->memory_needed, system);
 			remove_rr(round_robin, removed_p);
 		
@@ -304,40 +354,53 @@ void find_hole(system_t* system, process_t* process, queue_t* memory,
 		
 	}
 	
-	heapsort_t(removed, num_disk++, p_sort);
+	if(num_disk){
+		
+		heapsort_t(removed, num_disk, p_sort);
 	
-	for(i = 0; i < num_disk; i++){
-		add_disk(removed[i], disk);
+		for(i = 0; i < num_disk; i++){
+			add_disk(removed[i], disk);
+		}
 	}
 	
 	free(removed);
+	debug(0, "__find_hole end");
 }
 
 //add hole at location address and of size size
 void add_hole(int address, int size, system_t* system){
 	int num = system->num_holes;
 	
+	debug(0, "__add_hole start");
 	(system->memholes)[num] = (memhole_t*)malloc(sizeof(memhole_t));
 	null_check((system->memholes)[num], 1);
 	system->num_holes++;
 	
 	(system->memholes)[num]->address = address;
+	debug(address, "		hole address");
+	debug(size, "		hole size");
 	(system->memholes)[num]->size = size;
 	
-	num++;
-	heapsort_t(system->memholes, num, first_fit);
+	heapsort_t(system->memholes, system->num_holes, first_fit);
+	
 	//check for contiguity
+	print_holes(system->memholes, system->num_holes);
 	check_contiguity(system);
+	print_holes(system->memholes, system->num_holes);
+	
 	//sort list back into desired order
-	heapsort_t(system->memholes, num, system->sort_list);
+	heapsort_t(system->memholes, system->num_holes, system->sort_list);
+	debug(0,"__add_hole end");
 }
 
 //check for contiguous holes and combine
 void check_contiguity(system_t* system){
 	int i = 0;
 	
+	debug(0, "__check_contiguity start");
 	while(i < system->num_holes - 1){
-		if((system->memholes)[i]->address - (system->memholes)[i]->size ==
+		
+		if((system->memholes)[i]->address - (system->memholes)[i]->size == 
 			(system->memholes)[i+1]->address){
 			
 			(system->memholes)[i]->size += (system->memholes)[i+1]->size;
@@ -348,37 +411,45 @@ void check_contiguity(system_t* system){
 			i++;
 		}
 	}
+	debug(0, "__check_contiguity end");
 }
 
 //delete array entry in memholes
 void delete_hole(memhole_t** memholes, int index, int size){
 	int i;
 	
-	memholes[index] = NULL;
-	index++;
-	
-	for(i = index; i < size - 1; i++){
-		memholes[index - 1] = memholes[index];
+	debug(size, "__delete_hole start, size is");
+	for(i = index; i < size -1; i++){
+		memholes[i] = memholes[i + 1];
 	}
 	
-	memholes[index] = NULL;
-	
+	memholes[i] = NULL;
+	debug(0, "__delete_hole finish");
 }
 
 //add a process to the end of the disk queue
 void add_disk(process_t* process, queue_t* disk){
 	
+	debug(0, "__add_disk start");
 	if(!null_check(disk->start, 0) && !null_check(disk->end, 0)){
+		debug(0, "NULL disk");
 		disk->start = process;
 		disk->end = process;
+		debug(0, "end/start assigned");
+		debug(process->id, "	Current process is: ");
 		process->next_disk = NULL;
+		debug(0, "next_disk assigned");
+		
 	}
 	
 	else{
+		debug(0, "		add to end of queue");
 		disk->end->next_disk = process;
 		disk->end = process;
+		debug(null_check(process, 0), "		process null status");
 		process->next_disk = NULL;
 	}
+	debug(0, "__add_disk end");
 }
 
 //remove given process from round robin chronological listing
@@ -388,10 +459,11 @@ void remove_rr(queue_t* queue, process_t* process){
 	current = queue->start;
 	previous = NULL;
 	
+	debug(0, "__remove_rr starts");
 	while(current){
 		
 		if(current->id == process->id){
-			
+			debug(current->id, "		process id ");
 			//check if start of queue
 			if(null_check(previous, 0)){
 				previous->next = current->next;
@@ -402,7 +474,9 @@ void remove_rr(queue_t* queue, process_t* process){
 			}
 			if (!null_check(current->next, 0)){
 				queue->end = previous;
-				previous->next = NULL;
+				if(null_check(previous, 0)){
+						previous->next = NULL;
+				}
 			}
 			current->address = -1;
 			current->next = NULL;
@@ -413,19 +487,28 @@ void remove_rr(queue_t* queue, process_t* process){
 		current = current->next;
 	}
 	
+	debug(0, "__remove_rr ends");
 	queue->num_inqueue--;
 }
 
 //calculate memusage
 int memusage(queue_t* memory, system_t* system){
 	process_t* current = memory->start;
-	int usage = 0;
+	double usage = 0;
+	int percentage;
 	
 	while(current){
 		usage += current->memory_needed;
+		current = current->next_mem;
 	}
 	
-	return usage/system->max_memory;
+	percentage = (int)(100*usage)/system->max_memory;
+	
+	if((100*usage)/system->max_memory - (double)percentage > 0.00){
+		percentage++;
+	}
+	
+	return percentage;
 	
 	
 }
@@ -433,6 +516,7 @@ int memusage(queue_t* memory, system_t* system){
 //adds process ready to go on disk to array for sorting
 process_t** p_array(process_t* process, process_t** array, int index, int* max){
 	
+	debug(0, "__p_array starts");
 	if( index >= *max){
 		(*max)*=2;
 		array = (process_t**)realloc(array, sizeof(process_t*)*(*max));
@@ -440,5 +524,40 @@ process_t** p_array(process_t* process, process_t** array, int index, int* max){
 	
 	array[index] = process;
 	
+	debug(0, "__p_array ends");
 	return array;
+}
+
+//set all queue values to NULL and 0
+void initialise_queue(queue_t* queue){
+	queue->start = NULL;
+	queue->end = NULL;
+	queue->num_inqueue = 0;
+}
+
+//print all memory holes
+void print_holes(memhole_t** memholes, int size){
+	int i;
+	
+	if(DEBUG){
+		printf("\n\n");
+		for(i = 0; i < size; i++){
+			printf("Hole Address: %d, Hole Size: %d\n", memholes[i]->address,
+				memholes[i]->size);
+		}
+	}
+}
+
+//print round robine queue
+void print_rr(queue_t* queue){
+	process_t* current = queue->start;
+	
+	if(DEBUG){
+		printf("\n\n");
+		while(current){
+			printf("Process Address: %d, Process Size: %d, Process ID: %d\n", 
+				current->address,current->memory_needed, current->id);
+			current = current->next;
+		}
+	}
 }
